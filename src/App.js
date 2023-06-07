769f5eec6b8bc431581a87cd46f97206f06428ad
shubham jain <shubhamjain244122@gmail.com>
2023-06-07 23:40:56 +0530

    new commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <br />
+        <br />
+        "तत्त्वार्थसूत्र ज्ञानसम्पादनं, कर्मसम्यक्त्वाद् उत्तमं प्राप्ति।
+        अन्तःकरणशुद्धि प्रभवति, नैर्मल्येण देवस्य दर्शनम्॥"
+        <br />
+        shubhaarambh
+        <br />
+        "Commit to the Lord whatever you do, and your plans will succeed".
+        Proverbs 16:3
+      </div>
+    </div>
+  );
+}
+
+export default App;
